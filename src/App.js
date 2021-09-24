7834b0eff8d101ecc359e5e3f254600cf059dd07
Cristian23988 <cfbravo@unimayor.edu.co>
2021-09-23 19:19:04 -0500

    Router
    Routeo de paginas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-
+import GestVend from './ListaVentas.js';
+import React from "react";
+import { Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> .
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <Link to="/pag" >Gestion de vendedores</Link> 
+        
+
+      </div>
+
+
+
     </div>
   );
 }
---
+++ src/ListaVentas.js
@@ -0,0 +1,103 @@
+import logo from './logo.svg';
+import './css.css';
+import React from "react";
+   import {Link } from "react-router-dom";
+
+function GestVend() {
+    return (
+        <div className="GestVend">
+            <header className="GestVend-header">
+           
+
+            <body>
+                <header>
+
+                    <div id="logo"></div>
+                    <div id="User" class="redes1"><h6>Admin 1</h6></div>
+                    <div id="icono3" class="redes" >
+
+                        <image src="./User.png" lass="rounded mx-auto d-block" alt="..." width="42px"></image>
+                    </div>
+
+                </header>
+
+
+                <section>
+
+                    <aside id="izq"></aside>
+
+                    <div class="container">
+
+
+                        <h3>Lista de vendedores</h3>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Nombre</th>
+                                    <th scope="col">Apellido</th>
+                                    <th scope="col">N°Documento</th>
+                                    <th scope="col">Telefono</th>
+                                    <th scope="col">Dirección</th>
+                                    <th scope="col">Estado</th>
+                                    <th scope="col">Actualizar</th>
+                                    <th scope="col">Eliminar</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Manuel </td>
+                                    <td>Silva </td>
+                                    <td>123456780</td>
+                                    <td>3100000000</td>
+                                    <td>Carrera # </td>
+                                    <td>Activo </td>
+                                    <td><button type="button" class="btn btn-warning">Editar<a href="./FormH7.html"> </a></button></td>
+                                    <td><button type="button" class="btn btn-danger"> X </button></td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>Diego</td>
+                                    <td>Diaz</td>
+                                    <td>1234567890</td>
+                                    <td>3100000001</td>
+                                    <td>Carrera # </td>
+                                    <td>Activo </td>
+                                    <td><button type="button" class="btn btn-warning">Editar </button></td>
+                                    <td><button type="button" class="btn btn-danger"> X </button></td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">3</th>
+                                    <td >Larry</td>
+                                    <td >the Bird</td>
+                                    <td>1234567890</td>
+                                    <td>3100000001</td>
+                                    <td>Carrera # </td>
+                                    <td>Activo </td>
+                                    <td><button type="button" class="btn btn-warning">Editar </button></td>
+                                    <td><button type="button" class="btn btn-danger"> X </button></td>
+
+                                </tr>
+                            </tbody>
+                        </table>
+
+                    </div>
+
+                    <aside id="der"></aside>
+                    <span id="to"></span>
+                </section>
+
+
+
+
+
+
+            </body>
+            </header>
+            
+        </div>
+    );
+}
+
+export default GestVend;
