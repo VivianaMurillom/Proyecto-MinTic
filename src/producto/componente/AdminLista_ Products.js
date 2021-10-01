64818cd7442a005409d691dfdf47eb33cd3ccb04
Cristian23988 <cfbravo@unimayor.edu.co>
2021-10-01 01:20:04 -0500

    ELIMINAR PRODUCTOS
    Eliminar productos

--- src/producto/componente/AdminLista_ Products.js
+++ src/producto/componente/AdminLista_ Products.js
@@ -1,28 +1,35 @@
 import { Link } from "react-router-dom";
-import React,{useState} from "react";
-import UpdateProd from './UpdateProd';
+import React, { useState } from "react";
+
 const AdminLista_Products = ({ prod }) => {
-    
-    const remove=(num)=>{
-        if(prod.id==num){
-            prod.nombreP="Actualizado"
-        }
+    const  [ value , setValue ]  = useState ( 1 ) ;
 
-        
-        }
+    const EliminarItem = idSeleccionado => {
+        const newList = prod.filter((item) => item.id !== idSeleccionado)
+        prod.splice(0, prod.length);
+        newList.map((newPro)=>{
+            prod.push({
+                id: newPro.id,
+                nombreP: newPro.nombreP,
+                cantidad: newPro.cantidad,
+                precio: newPro.precio,
+            })
+        })
+        setValue ( ( value +  1 ) )
+        console.log("ELIMINADO", idSeleccionado,newList)
+    }
     return (
-        <tr>
-            <th scope="row">1</th>
-            <td>{prod.nombreP}</td>
-            <td>{prod.cantidad}</td>
-            <td>{prod.precio}</td>
-            <td><Link to="/updateVd" ><button type="button" class="btn btn-warning">Editar</button></Link></td>
-            <td><button class="btn btn-danger" onClick={remove(1)}>Eliminar</button></td>
-
-
-        </tr>
+        prod.map((proda) => {
+            return (
+                <tr>
+                    <th scope="row">#</th>
+                    <td>{proda.nombreP}</td>
+                    <td>{proda.cantidad}</td>
+                    <td>{proda.precio}</td>
+                    <td><Link to="/updateVd" id={proda.id}><button type="button" class="btn btn-warning">Editar</button></Link></td>
+                    <td><button class="btn btn-danger" onClick={() => EliminarItem(proda.id)}>Eliminar</button></td>
+                </tr>)
+        })
     )
 }
-
-
 export default AdminLista_Products
