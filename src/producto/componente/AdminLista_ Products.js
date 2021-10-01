71544386305a2269c3f6558b8d57fb43449a62c8
Cristian23988 <cfbravo@unimayor.edu.co>
2021-09-30 20:51:28 -0500

    a

--- src/producto/componente/AdminLista_ Products.js
+++ src/producto/componente/AdminLista_ Products.js
@@ -1,22 +1,27 @@
 import { Link } from "react-router-dom";
-const AdminLista_Products =({prod})=>{
-
-
-        return (
-        <tr>
-        <th scope="row">1</th>
-        <td>{prod.nombreP}</td>
-        <td>{prod.cantidad}</td>
-        <td>{prod.precio}</td>
-        <td><Link to="/updateVd" ><button type="button" class="btn btn-warning">Editar</button></Link></td>
-        <td><button type="button" id={prod.id} class="btn btn-danger"> X </button></td>
-    </tr>
-    )
+import React,{useState} from "react";
+import UpdateProd from './UpdateProd';
+const AdminLista_Products = ({ prod }) => {
     
-     
+    const remove=(num)=>{
+        if(prod.id==num){
+            prod.nombreP="Actualizado"
+        }
+
         
+        }
+    return (
+        <tr>
+            <th scope="row">1</th>
+            <td>{prod.nombreP}</td>
+            <td>{prod.cantidad}</td>
+            <td>{prod.precio}</td>
+            <td><Link to="/updateVd" ><button type="button" class="btn btn-warning">Editar</button></Link></td>
+            <td><button class="btn btn-danger" onClick={remove(1)}>Eliminar</button></td>
 
 
+        </tr>
+    )
 }
 
 
