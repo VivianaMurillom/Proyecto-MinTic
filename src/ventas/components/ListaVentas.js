fb91b57c1be15f212e8df1601fb0ff2488c1ff2d
Cristian23988 <cfbravo@unimayor.edu.co>
2021-10-02 11:56:53 -0500

    a

--- src/ventas/components/ListaVentas.js
+++ src/ventas/components/ListaVentas.js
@@ -36,8 +36,9 @@ const ListaVentas = ({ ventas }) => {
             <td>{vent.total}</td>
 
 
-            <td><button type="button"  className="btn btn-danger" onClick={() => EliminarItem(vent.id)}> X </button></td>  
+            
             <td><Link to="/updateVentas" ><button className="btn btn-warning" onClick={()=>UpdateItem(vent.id)}>Editar</button></Link></td>
+            <td><button type="button"  className="btn btn-danger" onClick={() => EliminarItem(vent.id)}> Eliminar</button></td>  
  
             </tr>
         ))
